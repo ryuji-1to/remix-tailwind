9b7de875a2e6ff1a4670dc3f6b5195e3b040fd15
RyujiIto <ryuji0222ss@gmail.com>
2021-12-08 13:36:01 +0900

    🔧 add a11y rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',

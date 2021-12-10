83e0dfa7ec38aa6c117a365494ffc57507a3bbf5
RyujiIto <ryuji0222ss@gmail.com>
2021-12-10 20:47:22 +0900

    📝

--- app/routes/about/sample.tsx
+++ app/routes/about/sample.tsx
@@ -1,5 +1,13 @@
-import type { LoaderFunction, MetaFunction } from 'remix';
-import { Link, useLoaderData } from 'remix';
+import type { ActionFunction, LoaderFunction, MetaFunction } from 'remix';
+import { Form, Link, useLoaderData, useTransition } from 'remix';
+
+import { sleep } from '~/lib';
+
+export const action: ActionFunction = async () => {
+  await sleep(1000);
+
+  return null;
+};
 
 export const loader: LoaderFunction = async () => {
   return 'This is a sample page';
@@ -14,6 +22,7 @@ export const meta: MetaFunction = () => {
 
 export default function SampleRoute() {
   const data = useLoaderData();
+  const transition = useTransition();
 
   return (
     <div>
@@ -21,6 +30,17 @@ export default function SampleRoute() {
       <Link to="/" className="underline">
         go back to home
       </Link>
+      <Form method="post">
+        <p>
+          <button
+            type="submit"
+            disabled={!!transition.submission}
+            className="p-2 text-lg font-bold text-white bg-purple-500 rounded-lg hover:bg-purple-600"
+          >
+            {transition.submission ? 'submitting...' : 'submit'}
+          </button>
+        </p>
+      </Form>
     </div>
   );
 }
